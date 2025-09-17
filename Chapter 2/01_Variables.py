706d57951d931b2d69c10592dd3aa1b9c3b8c45d
MahfuzurRahman-dev <mahfuz25812@gmail.com>
2025-09-17 23:40:28 +0600

    organize chapters, learn operators

---
+++ Chapter 2/01_Variables.py
@@ -0,0 +1,9 @@
+a = 1
+
+b = 2
+
+c = 7
+
+d = "Mahfuz"
+
+print (a + b + c + d)
---
+++ Chapter 2/03_rules_variables.py
@@ -0,0 +1,10 @@
+a = 1 
+
+aaa = 425
+
+harry = 100 
+
+_sameer = 500
+
+# @sameer = 700 # invalid due to @ symbol
+# s@meer  # invalid due to @ symbol
